de615efe70a991dcbd7ca2ec6a1162b6343821ef
Philip Lan <philiplan2017@u.northwestern.edu>
2016-11-19 01:15:48 -0600

    first get request in frontend

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,6 +1,11 @@
 (function(){
     angular.module("leagueOfScrubs")
     .controller("HomeController",["$scope", "$state", "$http", function($scope, $state, $http){
-        $scope.name = "Scrubs";
+        $scope.getGames = function (summonerName, region) {
+            var url = "/api/usergames/" + summonerName + "/" + region;
+            $http.get(url).success(function(data){
+                console.log(data);
+            });
+        }
     }]);
 }());
